fe6525b9571d773d7247064e948f6630b70b38da
giuliojiang <giuliojiang@gmail.com>
2018-12-09 18:21:33 +0000

    Implement list of albums

--- www/elements/hub.js
+++ www/elements/hub.js
@@ -4,14 +4,16 @@ window.customElements.define("metalpic-hub", class extends HTMLElement {
     constructor() {
         super();
         this.renderFirst();
+        this.data = null; // {albums: [{name, public, created}]}
     }
 
     connectedCallback() {
+        this.data = null;
         this.render();
+        this.requestAlbums();
     }
 
     // Events =================================================================
-
 
     handleButtonClick() {
         let event = new Event("metalpic-routechange");
@@ -26,11 +28,14 @@ window.customElements.define("metalpic-hub", class extends HTMLElement {
         this.shadow = this.attachShadow({mode: "open"});
         this.shadow.innerHTML = `
         <style>
-
+        .container {
+            padding: 10px;
+        }
         </style>
         `;
         this.body = document.createElement("div");
         this.shadow.appendChild(this.body);
+        this.body.classList.add("container");
     }
 
     render() {
@@ -46,6 +51,40 @@ window.customElements.define("metalpic-hub", class extends HTMLElement {
             this.handleButtonClick();
         });
         uploadLink.innerText = "Upload";
+
+        if (this.data != null) {
+            let albumsDiv = document.createElement("div");
+            body.appendChild(albumsDiv);
+
+            for (let album of this.data.albums) {
+                let div = document.createElement("div");
+                albumsDiv.appendChild(div);
+                let name = document.createElement("p");
+                div.appendChild(name);
+                name.innerText = album.name;
+                let p = document.createElement("p");
+                div.appendChild(p);
+                p.innerText = album.public ? "public" : "private";
+                let created = document.createElement("p");
+                div.appendChild(created);
+                created.innerText = new Date(album.created).toString();
+            }
+        }
+    }
+
+    // Private ================================================================
+
+    async requestAlbums() {
+        let tokenComp = encodeURIComponent(jpress.gsignin.token);
+        let response = await fetch(`/list/${tokenComp}`, {
+            method: "GET"
+        });
+        console.info("Received response. Type is " + typeof response);
+        let obj = await response.json();
+        console.info(JSON.stringify(obj));
+        // {"albums":[{"name":"faser234","public":false,"created":1544376564304},{"name":"faser","public":false,"created":1544363853532}]}
+        this.data = obj;
+        this.render();
     }
 
 })
