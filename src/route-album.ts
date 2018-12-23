5109891567734ba22ec1bc79be23185dfcd39e2d
giuliojiang <giuliojiang@gmail.com>
2018-12-23 17:01:28 +0100

    Remove debug prints

--- src/route-album.ts
+++ src/route-album.ts
@@ -15,9 +15,7 @@ var albumHandler = function(): express.Express {
 
     app.post("/", async (req, res) => {
         let body = req.body;
-        logger.info("<><><> body is " + JSON.stringify(body));
         let album = await mongoAlbum.getAlbumByName(body.album);
-        logger.info("<><><> got album from db " + JSON.stringify(album));
 
         // If album is private, user needs to be admin
         if (!album.public) {
@@ -31,7 +29,6 @@ var albumHandler = function(): express.Express {
 
         // Get the pictures in the album
         let pictures = await mongoPic.getPicturesInAlbum(album._id);
-        console.info("<><><> pictures in album are " + JSON.stringify(pictures));
         let result: any = {
         };
         result.pictures = [];
