f6d67f92516114170eccdf2cef37061dd2d19f1a
giuliojiang <giuliojiang@gmail.com>
2018-12-24 12:32:07 +0100

    Display list of albums in the control panel

--- www/elements/control-panel-content.js
+++ www/elements/control-panel-content.js
@@ -4,10 +4,40 @@ window.customElements.define("metalpic-control-panel-content", class extends HTM
 
     constructor() {
         super();
+        this.albumsData = null; // {albums: [{name, public, created}]}
     }
 
     connectedCallback() {
-        this.innerHTML = `<p>Hello from control-panel-content</p>`;
+        this.renderFirst();
+        this.loadAlbums();
+    }
+
+    async loadAlbums() {
+        let httpResult = await fetch(`/list/${encodeURIComponent(localStorage.token)}`, {
+            method: "GET"
+        });
+        this.albumsData = await httpResult.json();
+        this.render();
+    }
+
+    renderFirst() {
+        this.innerHTML = `
+            <style>
+
+            </style>
+            <div data-body></div>
+        `;
+    }
+
+    render() {
+        let body = this.querySelector("[data-body]");
+
+        for (let album of this.albumsData.albums) {
+            // album: {name, public, created}
+            let div = document.createElement("div");
+            body.appendChild(div);
+            div.innerText = album.name;
+        }
     }
 
 })
