2b08a4e4d1e6a021343de68cdca9b9d23553074d
giuliojiang <giuliojiang@gmail.com>
2018-12-27 11:02:09 +0000

    Use webpack for browser side js

---
+++ www/src/index.js
@@ -0,0 +1,17 @@
+import "@webcomponents/webcomponentsjs/webcomponents-bundle";
+
+import "./lib/metalpic";
+import "./lib/util";
+
+import "./elements/album";
+import "./elements/control-panel-album";
+import "./elements/control-panel";
+import "./elements/hub-buttons";
+import "./elements/hub";
+import "./elements/login";
+import "./elements/navbar";
+import "./elements/picture-preview";
+import "./elements/root";
+import "./elements/router";
+import "./elements/upload";
+
